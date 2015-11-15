df22a7f002351d290cf680fcfea16e115cff1264
Tobias Savinainen <tobias@savinainen.se>
2015-11-15 21:10:53 +0100

    Added relay command class

---
+++ TvShowManagerWPF/RelayCommand.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TvShowManagerWPF
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action targetExecuteMethod;
+        private readonly Func<bool> targetCanExecuteMethod;
+
+        public RelayCommand(Action executeMethod)
+        {
+            targetExecuteMethod = executeMethod;
+        }
+
+        public RelayCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            targetExecuteMethod = executeMethod;
+            targetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (targetCanExecuteMethod != null)
+            {
+                return targetCanExecuteMethod();
+            }
+
+            return targetExecuteMethod != null;
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        void ICommand.Execute(object parameter)
+        {
+            targetExecuteMethod?.Invoke();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> targetExecuteMethod;
+        private readonly Func<T, bool> targetCanExecuteMethod;
+
+        public RelayCommand(Action<T> executeMethod)
+        {
+            targetExecuteMethod = executeMethod;
+        }
+
+        public RelayCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            targetExecuteMethod = executeMethod;
+            targetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (targetCanExecuteMethod != null)
+            {
+                var tparm = (T)parameter;
+                return targetCanExecuteMethod(tparm);
+            }
+
+            return targetExecuteMethod != null;
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        void ICommand.Execute(object parameter)
+        {
+            targetExecuteMethod?.Invoke((T)parameter);
+        }
+    }
+}
