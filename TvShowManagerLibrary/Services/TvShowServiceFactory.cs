c0d8956d1d4da66a12acc48831f9b89609f9fee4
Tobias Savinainen <tobias@savinainen.se>
2015-11-15 21:05:50 +0100

    Added service factory

---
+++ TvShowManagerLibrary/Services/TvShowServiceFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TvShowManagerLibrary.ExternalServices;
+using TvShowManagerLibrary.Repositories;
+
+namespace TvShowManagerLibrary.Services
+{
+    public static class TvShowServiceFactory
+    {
+        public static TvShowService Create(string apiKey, string filename)
+        {
+            var externalService = new TvShowTMDbExternalService(apiKey);
+            var repository = new TvShowXmlRepository(filename);
+
+            return new TvShowService(externalService, repository);
+        }
+    }
+}
