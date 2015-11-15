a36c1d28e3dbffd2acf57dbbec93005783738e94
Tobias Savinainen <tobias@savinainen.se>
2015-11-15 19:37:12 +0100

    Added more extensions

---
+++ CoreLibrary/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary
+{
+    public static class ListExtensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this List<T> collection)
+        {
+            return new ObservableCollection<T>(collection);
+        } 
+    }
+}
---
+++ CoreLibrary/ObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNull(this object obj)
+        {
+            return obj == null;
+        }
+
+        public static bool IsNotNull(this object obj)
+        {
+            return obj != null;
+        }
+    }
+}
