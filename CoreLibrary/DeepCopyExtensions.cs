f006243bebb3d48d0d2d8b0c8a504d7b0f943268
Tobias Savinainen <tobias@savinainen.se>
2015-12-04 20:32:13 +0100

    Added extension methods

---
+++ CoreLibrary/DeepCopyExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary
+{
+    public static class DeepCopyExtensions
+    {
+        public static T DeepClone<T>(this T source)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, source);
+                stream.Position = 0;
+
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
